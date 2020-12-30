e7ad45a80a4b7b3d93d310cb0d21b5a07ccf793d
WendriTP <wendritrip4321@gmail.com>
2020-12-30 10:34:41 +0700

    Navigation

--- lib/shared/shared_value.dart
+++ lib/shared/shared_value.dart
@@ -2,3 +2,5 @@ part of 'shared.dart';
 
 String apiKey = "a7be2ba2d082f51042b496adce06cfdf";
 String imageBaseURL = "https://image.tmdb.org/t/p/";
+
+PageEvent prevPageEvent;
---
+++ lib/ui/pages/main_page.dart
@@ -0,0 +1,37 @@
+part of 'pages.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            BlocBuilder<UserBloc, UserState>(
+              builder: (_, userState) => (userState is UserLoaded)
+                  ? Text(
+                      userState.user.name,
+                      style: TextStyle(color: Colors.pink),
+                    )
+                  : SizedBox(),
+            ),
+            RaisedButton(
+                child: Text("Sign Out"),
+                onPressed: () {
+                  AuthServices.signOut();
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
